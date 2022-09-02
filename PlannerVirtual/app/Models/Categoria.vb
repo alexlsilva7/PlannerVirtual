d40a5910f2f8c4353b7ac40cc5bfa7adcaab022a
Alex <alex.lsilva154@gmail.com>
2022-09-02 14:53:33 -0300

    Criação da classe Categoria

---
+++ PlannerVirtual/app/Models/Categoria.vb
@@ -0,0 +1,30 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class Categoria
+    Private _nome As String
+    Private _cor As Color
+
+    Public Property nome() As String
+        Get
+            Return _nome
+        End Get
+        Set(ByVal value As String)
+            _nome = value
+        End Set
+    End Property
+
+    Public Property cor() As Color
+        Get
+            Return _cor
+        End Get
+        Set(ByVal value As Color)
+            _cor = value
+        End Set
+    End Property
+
+    Public Sub New(ByVal nome As String, ByVal cor As Color)
+        _nome = nome
+        _cor = cor
+    End Sub
+
+End Class
