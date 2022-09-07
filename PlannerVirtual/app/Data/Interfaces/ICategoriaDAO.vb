90fdceb777aac14cae8336b34f2be6006a397dd2
alexlsilva7 <alex.lsilva154@gmail.com>
2022-09-07 16:03:04 -0300

    adicionado interface ICategoriaDAO

---
+++ PlannerVirtual/app/Data/Interfaces/ICategoriaDAO.vb
@@ -0,0 +1,8 @@
+﻿Public Interface ICategoriaDAO
+
+    Sub inserir(ByVal categoria As Categoria)
+    Sub deletar(ByVal id As String)
+    Function listar() As List(Of Categoria)
+    Function consultar(ByVal id As String) As Categoria
+
+End Interface
