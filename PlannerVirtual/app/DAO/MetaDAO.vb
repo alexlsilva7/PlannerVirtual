21696bb6ddf7d26f28d9d78ab88089047c3333fb
JoseDanielF <danielduartefilho.df@gmail.com>
2022-09-12 23:04:27 -0300

    Adicionando classe MetaDAO

---
+++ PlannerVirtual/app/DAO/MetaDAO.vb
@@ -0,0 +1,25 @@
+Imports Microsoft.VisualBasic
+
+Public Class MetaDAO
+	'Banco TODO
+
+	Public Function inserir(ByRef meta: meta)
+		'Add - Meta	
+	End Function
+
+	Public Function deletar(ByVal id As String)
+		'Delete - Meta[id]
+	End Function
+
+	Public Function listar()
+		'Meta[*]
+	End Function
+
+	Public Function consultar(ByVal id As String)
+		'Meta[id]
+	End Function
+
+
+
+
+End Class
