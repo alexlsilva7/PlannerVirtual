4e6f5c552d22b341574b5cd69805e376a26eb5a5
JoseDanielF <danielduartefilho.df@gmail.com>
2022-09-12 23:01:59 -0300

    Adicionando classe TarefaDAO

---
+++ PlannerVirtual/app/DAO/TarefaDAO.vb
@@ -0,0 +1,25 @@
+Imports Microsoft.VisualBasic
+
+Public Class TarefaDAO
+	'Banco TODO
+
+	Public Function inserir(ByRef Tarefa: Tarefa)
+		'Add - Tarefa	
+	End Function
+
+	Public Function deletar(ByVal id As String)
+		'Delete - Tarefa[id]
+	End Function
+
+	Public Function listar()
+		'Tarefa[*]
+	End Function
+
+	Public Function consultar(ByVal id As String)
+		'Tarefa[id]
+	End Function
+
+
+
+
+End Class
