0abc280c4ada294bed26f172ec5e59c4c5d116da
JoseDanielF <danielduartefilho.df@gmail.com>
2022-09-12 23:03:21 -0300

    Adicionando classe CategoriaDAO

---
+++ PlannerVirtual/app/DAO/CategoriaDAO.vb
@@ -0,0 +1,25 @@
+Imports Microsoft.VisualBasic
+
+Public Class CategoriaDAO
+	'Banco TODO
+
+	Public Function inserir(ByRef categoria: categoria)
+		'Add - Categoria	
+	End Function
+
+	Public Function deletar(ByVal id As String)
+		'Delete - Categoria[id]
+	End Function
+
+	Public Function listar()
+		'Categoria[*]
+	End Function
+
+	Public Function consultar(ByVal id As String)
+		'Categoria[id]
+	End Function
+
+
+
+
+End Class
