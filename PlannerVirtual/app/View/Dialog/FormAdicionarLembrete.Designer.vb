ce88cdba4bfa1761ebe0cf3f4054112d683c853d
alexlsilva7 <alex.lsilva154@gmail.com>
2022-10-01 10:27:16 -0300

    Formulario Adicionar Lembrete

---
+++ PlannerVirtual/app/View/Dialog/FormAdicionarLembrete.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormAdicionarLembrete
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAdicionarLembrete = New System.Windows.Forms.Button()
+        Me.DatePicker = New System.Windows.Forms.DateTimePicker()
+        Me.txtDescricao = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblTipoLembrete = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnAdicionarLembrete
+        '
+        Me.btnAdicionarLembrete.Location = New System.Drawing.Point(26, 151)
+        Me.btnAdicionarLembrete.Name = "btnAdicionarLembrete"
+        Me.btnAdicionarLembrete.Size = New System.Drawing.Size(272, 23)
+        Me.btnAdicionarLembrete.TabIndex = 0
+        Me.btnAdicionarLembrete.Text = "Adicionar Lembrete"
+        Me.btnAdicionarLembrete.UseVisualStyleBackColor = True
+        '
+        'DatePicker
+        '
+        Me.DatePicker.Location = New System.Drawing.Point(68, 116)
+        Me.DatePicker.Name = "DatePicker"
+        Me.DatePicker.Size = New System.Drawing.Size(230, 20)
+        Me.DatePicker.TabIndex = 1
+        '
+        'txtDescricao
+        '
+        Me.txtDescricao.Location = New System.Drawing.Point(98, 75)
+        Me.txtDescricao.Name = "txtDescricao"
+        Me.txtDescricao.Size = New System.Drawing.Size(200, 20)
+        Me.txtDescricao.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(23, 78)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 16)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Descrição:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(23, 120)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Data:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(23, 27)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(107, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Novo Lembrete -"
+        '
+        'lblTipoLembrete
+        '
+        Me.lblTipoLembrete.AutoSize = True
+        Me.lblTipoLembrete.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTipoLembrete.Location = New System.Drawing.Point(129, 27)
+        Me.lblTipoLembrete.Name = "lblTipoLembrete"
+        Me.lblTipoLembrete.Size = New System.Drawing.Size(89, 16)
+        Me.lblTipoLembrete.TabIndex = 6
+        Me.lblTipoLembrete.Text = "tipo_lembrete"
+        '
+        'FormAdicionarLembrete
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(323, 191)
+        Me.Controls.Add(Me.lblTipoLembrete)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtDescricao)
+        Me.Controls.Add(Me.DatePicker)
+        Me.Controls.Add(Me.btnAdicionarLembrete)
+        Me.Name = "FormAdicionarLembrete"
+        Me.Text = "FormAdicionarLembrete"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnAdicionarLembrete As Button
+    Friend WithEvents DatePicker As DateTimePicker
+    Friend WithEvents txtDescricao As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblTipoLembrete As Label
+End Class
---
+++ PlannerVirtual/app/View/Dialog/FormAdicionarLembrete.vb
@@ -0,0 +1,41 @@
+﻿Public Class FormAdicionarLembrete
+    Public tipoLembrete As TipoLembrete
+    Private _lembreteDAO As ILembreteDAO
+
+    Private Sub FormAdicionarLembrete_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        initState()
+    End Sub
+
+    Private Sub initState()
+        _lembreteDAO = LembreteDAO.getSingletonObject
+        If (tipoLembrete = TipoLembrete.ligacoesImportantes) Then
+            lblTipoLembrete.Text = "Ligação Importante"
+        ElseIf (tipoLembrete = TipoLembrete.reunioes) Then
+            lblTipoLembrete.Text = "Reunião"
+        ElseIf (tipoLembrete = TipoLembrete.compras) Then
+            lblTipoLembrete.Text = "Compras"
+        End If
+    End Sub
+
+    Private Sub btnAdicionarLembrete_Click(sender As Object, e As EventArgs) Handles btnAdicionarLembrete.Click
+        Dim descricao As String = txtDescricao.Text
+        Dim data As Date = DatePicker.Value
+
+        Dim novoLembrete As New Lembrete(descricao, data, tipoLembrete)
+
+
+        If descricao <> "" Then
+            Try
+                _lembreteDAO.inserir(novoLembrete)
+                txtDescricao.ResetText()
+                Me.Close()
+            Catch ex As LembreteExistenteException
+                MsgBox("Lembrete já existente")
+            Catch ex As Exception
+                MsgBox("Erro ao adicionar lembrete: " & ex.ToString)
+            End Try
+        Else
+            MsgBox("Digite uma descrição para o lembrete para poder salvar!")
+        End If
+    End Sub
+End Class
