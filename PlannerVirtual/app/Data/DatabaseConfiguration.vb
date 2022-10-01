82873f5b1322cb73cde75e5f44c7cab61723aec4
alexlsilva7 <alex.lsilva154@gmail.com>
2022-10-01 17:50:08 -0300

    atualização de arquivos

--- PlannerVirtual/app/Data/DatabaseConfiguration.vb
+++ PlannerVirtual/app/Data/DatabaseConfiguration.vb
@@ -1,6 +1,6 @@
 ﻿Public Class DatabaseConfiguration
 
     Public Shared Function getConnectionString() As String
-        Return "Data Source= C:\Users\danie\OneDrive\Área de Trabalho\PlannerVirtual\PlannerVirtual\database.db; Version=3; New=True; Compress=True;"
+        Return "Data Source= C:\Users\Mr Robot\source\repos\alexlsilva7\PlannerVirtual\PlannerVirtual\database.db; Version=3; New=True; Compress=True;"
     End Function
 End Class
