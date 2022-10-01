621024934814ef88fde8c1e4c82f20c3344eaa09
alexlsilva7 <alex.lsilva154@gmail.com>
2022-10-01 17:43:12 -0300

    listview de lembretes tipo reunião

--- PlannerVirtual/app/View/FormLembrete.vb
+++ PlannerVirtual/app/View/FormLembrete.vb
@@ -4,13 +4,14 @@
     Private Sub FormLembrete_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         _lembreteDAO = LembreteDAO.getSingletonObject
         carregaDadosLigacoes()
+        carregaDadosReunioes()
     End Sub
 
     Private Sub carregaDadosLigacoes()
         'limpar listview
         listViewLigacoes.Items.Clear()
         'Busca os dados
-        Dim lista As List(Of Lembrete) = _lembreteDAO.listar()
+        Dim lista As List(Of Lembrete) = _lembreteDAO.listar(TipoLembrete.ligacoesImportantes)
         'Preencher o listview
         Try
             For Each lembrete As Lembrete In lista
@@ -22,11 +23,33 @@
         End Try
     End Sub
 
-    Private Sub btnAddLigacaoImportate_Click(sender As Object, e As EventArgs) Handles btnAddLigacaoImportate.Click
+    Private Sub carregaDadosReunioes()
+        'limpar listview
+        listViewReunioes.Items.Clear()
+        'Busca os dados
+        Dim lista As List(Of Lembrete) = _lembreteDAO.listar(TipoLembrete.reunioes)
+        'Preencher o listview
+        Try
+            For Each lembrete As Lembrete In lista
+                Dim item = New ListViewItem({lembrete.id.ToString, lembrete.descricao, lembrete.data})
+                listViewReunioes.Items.Add(item)
+            Next
+        Catch ex As Exception
+            MessageBox.Show("Erro ao carregar os dados: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnAddLigacaoImportate_Click(sender As Object, e As EventArgs)
         Dim formAdicionarLembrete As New FormAdicionarLembrete
         formAdicionarLembrete.tipoLembrete = TipoLembrete.ligacoesImportantes
         formAdicionarLembrete.ShowDialog()
         carregaDadosLigacoes()
     End Sub
 
+    Private Sub btnAddReuniao_Click(sender As Object, e As EventArgs) Handles btnAddReuniao.Click
+        Dim formAdicionarLembrete As New FormAdicionarLembrete
+        formAdicionarLembrete.tipoLembrete = TipoLembrete.reunioes
+        formAdicionarLembrete.ShowDialog()
+        carregaDadosReunioes()
+    End Sub
 End Class
