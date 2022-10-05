fbae8367e8af444bff309298d93a75d465875831
alexlsilva7 <alex.lsilva154@gmail.com>
2022-10-05 20:16:17 -0300

    adicionado FormMostrarTarefa

---
+++ PlannerVirtual/app/View/Dialog/FormMostrarTarefa.Designer.vb
@@ -0,0 +1,203 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormMostrarTarefa
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblTitulo = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblDescricao = New System.Windows.Forms.Label()
+        Me.lblData = New System.Windows.Forms.Label()
+        Me.lblDuracao = New System.Windows.Forms.Label()
+        Me.lblEstado = New System.Windows.Forms.Label()
+        Me.lblCategoria = New System.Windows.Forms.Label()
+        Me.btnEditar = New System.Windows.Forms.Button()
+        Me.BtnApagar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(25, 182)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(81, 18)
+        Me.Label6.TabIndex = 34
+        Me.Label6.Text = "Categoria"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(25, 214)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(66, 18)
+        Me.Label5.TabIndex = 33
+        Me.Label5.Text = "Estado:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(25, 111)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(48, 18)
+        Me.Label4.TabIndex = 32
+        Me.Label4.Text = "Data:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(25, 146)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(77, 18)
+        Me.Label3.TabIndex = 31
+        Me.Label3.Text = "Duração:"
+        '
+        'lblTitulo
+        '
+        Me.lblTitulo.AutoSize = True
+        Me.lblTitulo.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitulo.ForeColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.lblTitulo.Location = New System.Drawing.Point(170, 9)
+        Me.lblTitulo.Name = "lblTitulo"
+        Me.lblTitulo.Size = New System.Drawing.Size(69, 24)
+        Me.lblTitulo.TabIndex = 30
+        Me.lblTitulo.Text = "Tarefa"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(25, 65)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(90, 18)
+        Me.Label1.TabIndex = 29
+        Me.Label1.Text = "Descrição:"
+        '
+        'lblDescricao
+        '
+        Me.lblDescricao.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDescricao.Location = New System.Drawing.Point(122, 67)
+        Me.lblDescricao.Name = "lblDescricao"
+        Me.lblDescricao.Size = New System.Drawing.Size(289, 38)
+        Me.lblDescricao.TabIndex = 35
+        Me.lblDescricao.Text = "Label2"
+        '
+        'lblData
+        '
+        Me.lblData.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblData.Location = New System.Drawing.Point(81, 113)
+        Me.lblData.Name = "lblData"
+        Me.lblData.Size = New System.Drawing.Size(289, 18)
+        Me.lblData.TabIndex = 36
+        Me.lblData.Text = "Label7"
+        '
+        'lblDuracao
+        '
+        Me.lblDuracao.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDuracao.Location = New System.Drawing.Point(108, 148)
+        Me.lblDuracao.Name = "lblDuracao"
+        Me.lblDuracao.Size = New System.Drawing.Size(289, 18)
+        Me.lblDuracao.TabIndex = 37
+        Me.lblDuracao.Text = "Label8"
+        '
+        'lblEstado
+        '
+        Me.lblEstado.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEstado.Location = New System.Drawing.Point(99, 216)
+        Me.lblEstado.Name = "lblEstado"
+        Me.lblEstado.Size = New System.Drawing.Size(289, 18)
+        Me.lblEstado.TabIndex = 38
+        Me.lblEstado.Text = "Label9"
+        '
+        'lblCategoria
+        '
+        Me.lblCategoria.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCategoria.Location = New System.Drawing.Point(122, 182)
+        Me.lblCategoria.Name = "lblCategoria"
+        Me.lblCategoria.Size = New System.Drawing.Size(289, 18)
+        Me.lblCategoria.TabIndex = 39
+        Me.lblCategoria.Text = "Label10"
+        '
+        'btnEditar
+        '
+        Me.btnEditar.Location = New System.Drawing.Point(27, 255)
+        Me.btnEditar.Name = "btnEditar"
+        Me.btnEditar.Size = New System.Drawing.Size(212, 23)
+        Me.btnEditar.TabIndex = 40
+        Me.btnEditar.Text = "Editar"
+        Me.btnEditar.UseVisualStyleBackColor = True
+        '
+        'BtnApagar
+        '
+        Me.BtnApagar.Location = New System.Drawing.Point(245, 255)
+        Me.BtnApagar.Name = "BtnApagar"
+        Me.BtnApagar.Size = New System.Drawing.Size(166, 23)
+        Me.BtnApagar.TabIndex = 41
+        Me.BtnApagar.Text = "Apagar"
+        Me.BtnApagar.UseVisualStyleBackColor = True
+        '
+        'FormMostrarTarefa
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(423, 290)
+        Me.Controls.Add(Me.BtnApagar)
+        Me.Controls.Add(Me.btnEditar)
+        Me.Controls.Add(Me.lblCategoria)
+        Me.Controls.Add(Me.lblEstado)
+        Me.Controls.Add(Me.lblDuracao)
+        Me.Controls.Add(Me.lblData)
+        Me.Controls.Add(Me.lblDescricao)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblTitulo)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Name = "FormMostrarTarefa"
+        Me.Text = "FormMostrarTarefa"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblTitulo As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblDescricao As Label
+    Friend WithEvents lblData As Label
+    Friend WithEvents lblDuracao As Label
+    Friend WithEvents lblEstado As Label
+    Friend WithEvents lblCategoria As Label
+    Friend WithEvents btnEditar As Button
+    Friend WithEvents BtnApagar As Button
+End Class
---
+++ PlannerVirtual/app/View/Dialog/FormMostrarTarefa.vb
@@ -0,0 +1,61 @@
+﻿Public Class FormMostrarTarefa
+    Public tarefa As Tarefa
+
+    Private _tarefaDAO As ITarefaDAO
+    Private Sub FormMostrarTarefa_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        _tarefaDAO = TarefaDAO.getSingletonObject()
+
+        inicializarLabels()
+    End Sub
+
+    Private Sub inicializarLabels()
+        lblDescricao.Text = tarefa.descricao
+
+        Select Case tarefa.duracao
+            Case 30
+                lblDuracao.Text = "30 min"
+                lblData.Text = tarefa.horarioInicio.ToString("dd/MM/yyyy HH:mm")
+            Case 60
+                lblDuracao.Text = "1h"
+                lblData.Text = tarefa.horarioInicio.ToString("dd/MM/yyyy HH:mm")
+            Case 240
+                If tarefa.horarioInicio.Hour = 8 Then
+                    lblDuracao.Text = "De manhã (8h às 12h)"
+                ElseIf tarefa.horarioInicio.Hour = 13 Then
+                    lblDuracao.Text = "De tarde (13h às 17h)"
+                ElseIf tarefa.horarioInicio.Hour = 18 Then
+                    lblDuracao.Text = "De noite (18h às 22h)"
+                End If
+                lblData.Text = tarefa.horarioInicio.ToString("dd/MM/yyyy")
+        End Select
+        lblCategoria.Text = tarefa.categoria.nome
+        lblCategoria.ForeColor = tarefa.categoria.cor
+        Select Case tarefa.estado
+            Case 0
+                lblEstado.Text = "A executar"
+            Case 1
+                lblEstado.Text = "Parcialmente executada"
+            Case 2
+                lblEstado.Text = "Adiada"
+            Case 3
+                lblEstado.Text = "Executada"
+        End Select
+    End Sub
+    Private Sub btnEditar_Click(sender As Object, e As EventArgs) Handles btnEditar.Click
+        Dim formAdicionarTarefa = New FormAdicionarTarefa
+        formAdicionarTarefa.tarefa = tarefa
+        formAdicionarTarefa.ShowDialog()
+        tarefa = formAdicionarTarefa.tarefa
+
+        inicializarLabels()
+    End Sub
+
+    Private Sub BtnApagar_Click(sender As Object, e As EventArgs) Handles BtnApagar.Click
+
+        Dim result As DialogResult = MessageBox.Show("Deseja apagar a tarefa: " & tarefa.descricao & " ?", "Apagar Tarefa", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            _tarefaDAO.deletar(tarefa.id)
+            Me.Close()
+        End If
+    End Sub
+End Class
