8892033e40ef04e8164b255f3c56cb994a94c65e
Antonio Bezerra <antonio.bezerra1414@gmail.com>
2022-10-05 13:48:43 -0300

    Implementacao da classe RelatorioQuantidade

---
+++ PlannerVirtual/app/Models/Relatorio/RelatorioQuantidade.vb
@@ -0,0 +1,25 @@
+﻿Public Class RelatorioQuantidade
+    Inherits Relatorio
+
+    Public Sub New(dataInicio As Date, dataFim As Date)
+        MyBase.New(dataInicio, dataFim)
+    End Sub
+
+    Public Function calcularQuantidadeTarefasExecutadas() As Integer
+        Dim tarefas As List(Of Tarefa) = getTarefasExecutadas()
+        Return tarefas.Count
+    End Function
+
+    Public Function calcularQuantidadeMetasCumpridas() As Integer
+        Dim metas As List(Of Meta) = getMetasCumpridas()
+        Return metas.Count
+    End Function
+
+    Public Function calcularPorcentagemTarefasExecutadas() As Double
+        Return calcularQuantidadeTarefasExecutadas() / getQuantidadeDeTarefas()
+    End Function
+
+    Public Function calcularPorcentagemMetasCumpridas() As Double
+        Return calcularQuantidadeMetasCumpridas() / getQuantidadeDeMetas()
+    End Function
+End Class
